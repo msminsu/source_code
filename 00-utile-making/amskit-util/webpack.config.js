98f06cf7cfc4d1888ecac93ccb17510e9defb830
msminsu <msminsu@gmail.com>
2019-02-23 08:09:48 +0900

    renamed

--- 00-utile-making/amskit-util/webpack.config.js
+++ 00-utile-making/amskit-util/webpack.config.js
@@ -1,13 +1,8 @@
 // webpack.config.js
 module.exports = {
-    entry: './src/test.js',
+    entry: './src/index.js',
     output: {
       path: __dirname,  // 현재 디렉토리
       filename: './dist/amskit-util.js'
-    },
-    module: {
-      loaders: [
-       { test: /\.sass$/, loader: 'style!css!sass' }
-      ]
     }
   };
